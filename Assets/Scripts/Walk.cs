438486abf789c0bdc8f50e398e6232936f10aa0a
best062 <jantaramanee99@gmail.com>
2026-03-04 20:27:27 +0700

    Update Coding

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 
+
 public class Walk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(moveX, 0, moveZ);
+
+        if (moveDirection != Vector3.zero)
+        {
+            transform.forward = moveDirection;
+        }
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
